062e049e84c28e320b59545da171bc4796d49e45
DAGA <DAGA@DAGA-PC.eam.edu.co>
2018-12-06 16:34:25 -0500

    Organizacion banner y carousel

--- View/public/carousel.php
+++ View/public/carousel.php
@@ -1,3 +1,5 @@
+<script src="Resources/public/js/general/home.js" type="text/javascript"></script>
+
 <div class="intro intro-carousel">
     <div id="carousel" class="owl-carousel owl-theme">
         <div class="carousel-item-a intro-item bg-image" style="background-image: url(Resources/public/template/img/slide-1.jpg)">
@@ -69,5 +71,41 @@
                 </div>
             </div>
         </div>
+        
+    </div>
+</div>
+<div class="filtroBusquedaBanner">
+    <div class="row" style="padding-bottom: 0px !important;">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <table>
+                <tr>
+                    <td>
+                        <button id="btnArriendoFiltroHome" type="button" onclick="cambiarTipoOferta('arriendo', this.id);" class="btn btn-block btn-primary btn-lg transparencia botonFiltroBusquedaBanner">Arriendo</button>
+                    </td>
+                    <td>
+                        <button id="btnVentaFiltroHome" type="button" onclick="cambiarTipoOferta('venta', this.id);" class="btn btn-block btn-primary btn-lg transparencia botonFiltroBusquedaBanner">Venta</button>
+                    </td>
+                    <td>
+                        <button id="btnVacacionalFiltroHome" type="button" onclick="cambiarTipoOferta('vacacional', this.id);" class="btn btn-block btn-primary btn-lg transparencia botonFiltroBusquedaBanner">Vacacional</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" >
+                        <table style="width: 100%">
+                            <tr>
+                                <td style="width: 80%;">
+                                    <input id="txtSearchNeighborhoodCity" name="txtSearchNeighborhoodCity" 
+                                           class="form-control" type="text" value="" autocomplete="off"
+                                           placeholder="Ingrese el nombre del barrio o la ciudad" style="float: left"> 
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-block btn-primary btn-lg transparencia" onclick="rapidSearch();">Buscar</button>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>                    
     </div>
 </div>
